73b7cbc5750449804c355fd79c3b703c2a141d79
Sharif Md. Saad Galib <galib@therapservices.net>
2015-12-17 13:08:08 +0600

    Implemented calculating refraction direction

--- source/physics.cpp
+++ source/physics.cpp
@@ -1,5 +1,35 @@
 #include <physics.h>
-Vec3f reflect(const Vec3f &I, const Vec3f &N)
+
+Vec3f reflect(const Vec3f &incidentDirection, const Vec3f &normal)
 {
-    return I - 2 * I.dotProduct(N) * N;
+    return incidentDirection - 2 * incidentDirection.dotProduct(normal) * normal;
+}
+
+/* How to use */
+
+/* Pass all directions normalizing.
+ * Otherwise it shall not behave
+ * correctly.
+ */
+
+Vec3f refract(const Vec3f &incidentDirection, const Vec3f &normal, float refracitveIndex) {
+    float cosI = incidentDirection.dotProduct(normal);
+    float eta, etaT = refracitveIndex, etaI = 1.0;
+    Vec3f refractionNormal = normal;
+
+    if(cosI < 0) {
+        cosI *= -1;
+        refractionNormal *= -1;
+    } else {
+        std::swap(etaT, etaI);
+    }
+
+    eta = etaI / etaT;
+
+    double k = 1 - eta * eta * (1 - cosI * cosI);
+    if(k < 0) {
+        return 0;
+    } else {
+        return (eta * incidentDirection + (eta * cosI - sqrtf(k)) * refractionNormal).normalize();
+    }
 }
